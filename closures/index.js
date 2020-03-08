74328acb484b523fe6a9c305f60c5e0d41a7b10a
ysumit99 <=>
2020-03-09 00:54:57 +0530

    Using function expressions instead of IIFE to create closure

--- closures/index.js
+++ closures/index.js
@@ -38,7 +38,7 @@ var add10 = makeAdder(10);// => add10 is a closure
 console.log(add5(2));
 console.log(add10(2));
 
-//Emulating private methods through closures
+//Emulating private methods through closures using anonymous functions / IIFE / Module Design Pattern
 var counter = (function () {
     var privateCounter = 0;
     function changeBy(val) {
@@ -68,3 +68,39 @@ console.log(counter.value());
 
 counter.decrement();
 console.log(counter.value());
+
+//assigning the function to a variable to create different closures
+var makeCounter = function () {
+    var privateCounter = 0;
+    function changeBy(val) {
+        privateCounter += val;
+    }
+
+    return {
+        increment: function () {
+            changeBy(1);
+        },
+
+        decrement: function () {
+            changeBy(-1);
+        },
+
+        value: function () {
+            return privateCounter;
+        }
+    };
+};
+
+//creating several counters
+var counter1 = makeCounter();
+var counter2 = makeCounter();
+
+alert(counter1.value());  // 0.
+
+counter1.increment();
+counter1.increment();
+alert(counter1.value()); // 2.
+
+counter1.decrement();
+alert(counter1.value()); // 1.
+alert(counter2.value()); // 0.
