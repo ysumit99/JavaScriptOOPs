39aa3e76c647fa780159fee38fd25976ce32528a
ysumit99 <=>
2020-03-08 17:12:11 +0530

    Collision Detection added

--- object_building_practice/main.js
+++ object_building_practice/main.js
@@ -54,6 +54,20 @@ Ball.prototype.update = function () {
 
 }
 
+Ball.prototype.collisionDetect = function () {
+    for (let j = 0; j < balls.length; j++) {
+        if (!(this === balls[j])) {
+            const dx = this.x - balls[j].x;
+            const dy = this.y - balls[j].y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < this.size + balls[j].size) {
+                balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+            }
+        }
+    }
+}
+
 //Lets draw some balls
 
 let balls = [];
@@ -78,6 +92,7 @@ function loop() {
     for (let i = 0; i < balls.length; i++) {
         balls[i].draw();
         balls[i].update();
+        balls[i].collisionDetect();
     }
 
     requestAnimationFrame(loop);
