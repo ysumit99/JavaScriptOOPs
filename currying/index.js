cb92516b8e1dbbc5f4330350bb1d1d44861c3557
Sumit Yadav <ysumit99@gmail.com>
2020-03-12 19:13:52 +0530

    Currying example

--- currying/index.js
+++ currying/index.js
@@ -1,3 +1,5 @@
+/** Example 1 */
+
 var avg = function (...n) {
     let tot = 0;
     for (let i = 0; i < n.length; i++) {
@@ -19,3 +21,21 @@ var spiceUp = function (fn, ...n) {
 
 var doAvg = spiceUp(avg, 1, 2, 3);// <=== argument 'n'
 console.log(doAvg(4, 5, 6, 7)) // <=== argument 'm'
+
+//****************************************
+/** Example 2 */
+
+
+var sayWhat = function (a) {
+    return function (b) {
+        return function (c) {
+            console.log("saying " + a + " to " + b + " using " + c);
+        }
+    }
+}
+
+sayWhat('hello')('Friends')('JavaScript');
+
+var sayHi = sayWhat('hi');
+var sayHiToMe = sayHi('me');
+var sayHiToMeUsingPython = sayHiToMe('python');
