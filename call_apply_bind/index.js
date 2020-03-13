b5ce0a3d0cb6f65a026d17dfdb0e4ec2f3ca31c2
Sumit Yadav <ysumit99@gmail.com>
2020-03-13 13:10:31 +0530

    Call Apply and bind methods

--- call_apply_bind/index.js
+++ call_apply_bind/index.js
@@ -1,9 +1,20 @@
-var obj = { num: 2 };
+let obj = { num: 2 };
+let obj2 = { num: 5 };
 
+let addToThis = function (a, b, c) {
 
-var addToThis = function (a) {
-
-    return this.num + a;
+    return this.num + a + b + c;
 };
 
-console.log(addToThis.call(obj, 3));
+//call function
+console.log(addToThis.call(obj, 1, 2, 3)); //first parameter is this
+
+//apply function
+let arr = [1, 2, 3];
+console.log(addToThis.apply(obj, arr)); //instead of individual values we pass an array incase of apply.
+console.log(addToThis.apply(obj2, arr)); //instead of individual values we pass an array incase of apply.
+
+//bind function
+let bound = addToThis.bind(obj);
+console.dir(bound);
+console.log(bound(1, 2, 3)); //values are passed as individual values similar to call.
