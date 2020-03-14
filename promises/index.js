310aae92cafd3983854359b431d256dca3a634e4
Sumit Yadav <ysumit99@gmail.com>
2020-03-14 10:45:06 +0530

    More examples on Promise, Promise.all, Promise.race

--- promises/index.js
+++ promises/index.js
@@ -1,3 +1,4 @@
+//Example 1
 let promiseToStudyJavaScript = new Promise(function (resolve, reject) {
 
     //Studying JavaScript
@@ -15,4 +16,46 @@ promiseToStudyJavaScript.then(function (fromResolve) {
     console.log('JavaScript studying is ' + fromResolve);
 }).catch(function (fromReject) {
     console.log('JavaScript studying is ' + fromReject);
+});
+
+//Example 2
+
+let cleanRoom = function () {
+    return new Promise(function (resolve, reject) {
+        resolve(' Cleaned the Room');
+    });
+};
+
+let removeGarbage = function (message) {
+    return new Promise(function (resolve, reject) {
+        resolve(message + ' remove Garbage');
+    });
+};
+
+let winIcecream = function (message) {
+    return new Promise(function (resolve, reject) {
+        resolve(message + ' won Icecream');
+    });
+};
+
+cleanRoom().then(function (result) {
+    return removeGarbage(result);
+}).then(function (result) {
+    return winIcecream(result);
+}).then(function (result) {
+    console.log(result + ' finished');
+});
+
+/* 
+execute the promises all at once without waiting for
+each one of them to resolve one by one and do something once all them finish
+*/
+Promise.all([cleanRoom(), removeGarbage(), winIcecream()]).then(function () {
+    console.log('All tasks finished!');
+});
+
+
+/* Do something as soon as one of the promise is finished */
+Promise.race([cleanRoom(), removeGarbage(), winIcecream()]).then(function () {
+    console.log('One task is finished!');
 });
